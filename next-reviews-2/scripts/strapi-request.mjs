b64be1f1dbcbe4624f6b0cca31aae67bad2a5a17
Robert Gleason <l2ob122@gmail.com>
2023-10-15 21:35:24 -0700

    add qs library, changed what kind of information we want from the url

--- next-reviews-2/scripts/strapi-request.mjs
+++ next-reviews-2/scripts/strapi-request.mjs
@@ -1,8 +1,23 @@
 import {writeFileSync} from 'fs';
+import qs from 'qs';
 
-const url = 'http://localhost:1337/api/reviews'+'?populate=*';
+const url = 'http://localhost:1337/api/reviews' + '?' + qs.stringify({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    populate: {
+        image: {
+            fields: ['url', 'width', 'height', 'alt']
+        }
+    },
+    sort: 'publishedAt:desc',
+    pagination: {
+        pageSize: 6
+    }
+}, {encodeValuesOnly: true});
+
+console.log(url);
+
 const response = await fetch(url);
 const data = await response.json();
-const formatted =JSON.stringify(data, null, 2);
+const formatted = JSON.stringify(data, null, 2);
 const file = 'scripts/strapi-response.json';
 writeFileSync(file, formatted, 'utf8');
