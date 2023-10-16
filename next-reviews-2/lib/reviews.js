422c3b73ba05863251c085b324ddac8653cad25b
Robert Gleason <l2ob122@gmail.com>
2023-10-15 22:18:57 -0700

    MADE THE FIXES USE THIS ONE with image

--- next-reviews-2/lib/reviews.js
+++ next-reviews-2/lib/reviews.js
@@ -3,55 +3,44 @@ import { marked } from "marked";
 import matter from "gray-matter";
 import qs from "qs";
 
+const CMS_URL = "http://localhost:1337";
+
 export async function getFeaturedReviews() {
   const reviews = await getReviews();
   return reviews[0];
 }
 
-//    slug: 'hollow-knight',
-//     title: 'Hollow Knight',
-//     date: '2023-11-05',
-//     image: '/images/hollow-knight.jpg',
 export async function getReview(slug) {
+  const text = await readFile(`./content/reviews/${slug}.md`, "utf8");
+  const {
+    content,
+    data: { title, date, image },
+  } = matter(text);
+  const body = marked(content, { headersIds: false, mangle: false });
+  return { slug, title, date, image, body };
+}
+
+export async function getReviews() {
   const url =
-    "http://localhost:1337/api/reviews?" +
+    `${CMS_URL}/api/reviews?` +
     qs.stringify(
       {
         fields: ["slug", "title", "subtitle", "publishedAt"],
-        populate: {
-          image: {
-            fields: ["url", "width", "height", "alt"],
-          },
-        },
-        sort: "publishedAt:desc",
-        pagination: {
-          pageSize: 6,
-        },
+        populate: { image: { fields: ["url"] } },
+        sort: ["publishedAt:desc"],
+        pagination: { pageSize: 6 },
       },
       { encodeValuesOnly: true }
     );
-  console.log("getReview url", url);
+  console.log("getReviews:", url);
   const response = await fetch(url);
   const { data } = await response.json();
   return data.map(({ attributes }) => ({
     slug: attributes.slug,
     title: attributes.title,
-    subtitle: attributes.subtitle,
-    date: attributes.publishedAt,
+    date: attributes.publishedAt.slice(0, "YYYY-MM-DD".length),
+    image: CMS_URL + attributes.image.data.attributes.url,
   }));
-}
-
-export async function getReviews() {
-  const slugs = await getSlugs();
-  const reviews = [];
-  for (const slug of slugs) {
-    const review = await getReview(slug);
-    reviews.push(review);
-  }
-  // // Sort reviews by date
-  // reviews.sort((a, b) => b.date.localeCompare(a.date));
-
-  return reviews;
 }
 
 export async function getSlugs() {
